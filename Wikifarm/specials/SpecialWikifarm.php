a25d4a4c0eb9dde4f3c4f7e387745f9aaadfdbd3
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-07-14 14:49:58 +0200

    Update SpecialWikifarm.php

--- Wikifarm/specials/SpecialWikifarm.php
+++ Wikifarm/specials/SpecialWikifarm.php
@@ -15,32 +15,34 @@
  * @license WOCL-01+
  */
 
-$wgExtensionCredits['specialpage'][] = array(
-	'path' => __FILE__,
-	'name' => 'Wikifarm',
-	'description' => 'A Wikifarm Special Page',
-	'author' => 'Suriyaa Kudo', 'Ævar Arnfjörð Bjarmason'
-);
+class SpecialHelloWorld extends SpecialPage {
 
-$wgSpecialPages['Wikifarm'] = 'SpecialWikifarm';
-
-class SpecialWikifarm extends WikifarmSpecialPage {
-
+	/**
+	 * Initialize the special page.
+	 */
 	public function __construct() {
+		// A special page should at least have a name.
+		// We do this by calling the parent class (the SpecialPage class)
+		// constructor method with the name as first and only parameter.
 		parent::__construct( 'Wikifarm' );
 	}
 
 	/**
-	 * Show the page
+	 * Shows the page to the user.
+	 * @param string $sub: The subpage string argument (if any).
+	 *  [[Special:Wikifarm/subpage]].
 	 */
-	public function execute( $par = null ) {
-		if ( $this->including() ) {
-			$out = "I'm being included";
-		} else {
-			$out = "I'm being viewed as a Special Page";
-			$this->setHeaders();
-		}
+	public function execute( $sub ) {
+		$out = $this->getOutput();
 
-		$this->getOutput()->addWikiText( $out );
+		$out->setPageTitle( $this->msg( 'wikifarm-title' ) );
+
+		// Parses message from .i18n.php as wikitext and adds it to the
+		// page output.
+		$out->addWikiMsg( 'wikifarm-intro' );
+	}
+
+	protected function getGroupName() {
+		return 'other';
 	}
 }
