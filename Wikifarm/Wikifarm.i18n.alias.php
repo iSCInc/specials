e3a7a39c0f2d224f99d1d29ffc0654592fd52e50
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-07-14 14:41:50 +0200

    Create Wikifarm.i18n.alias.php

---
+++ Wikifarm/Wikifarm.i18n.alias.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Aliases for special pages of the Special:Wikifarm
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'Wikifarm' => array( 'Wikifarm' ),
+);
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'Wikifarm' => array( 'Wikifarm' ),
+);
